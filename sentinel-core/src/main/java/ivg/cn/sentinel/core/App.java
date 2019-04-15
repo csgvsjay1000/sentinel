abddcb7e2fbdda1ca4219c48aff6550517747168
csgvsjay1000 <254633397@qq.com>
2019-04-15 09:37:53 +0800

    add sentinel-core

---
+++ sentinel-core/src/main/java/ivg/cn/sentinel/core/App.java
@@ -0,0 +1,13 @@
+package ivg.cn.sentinel.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
